4de24bd6a10036d8b70f481ee5c951da75de32ff
AN-Fahmy <fahmyan@taibaconnect.com>
2026-01-18 12:21:37 +0200

    collapse footer

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,18 +1,33 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit } from '@angular/core'
 import { currentYear } from '../../common/constants'
 import { CategoryService } from '@core/services/category/category.service'
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap'
 
 @Component({
     selector: 'app-footer',
-    imports: [],
+    imports: [NgbCollapseModule],
     templateUrl: './footer.component.html',
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class FooterComponent implements OnInit {
   private readonly _CategoryService = inject(CategoryService)
   year = currentYear
-
   allCategories:any[] = []
+  isCollapsed = true;
+  stage = 1; // 1 | 2 | 3
+
+  showMore() {
+    if (this.stage < 3) {
+      this.stage++;
+    }
+  }
+
+  showLess() {
+    if (this.stage > 1) {
+      this.stage--;
+    }
+  }
+
 
   ngOnInit(): void {
     this.getAllCategories()
